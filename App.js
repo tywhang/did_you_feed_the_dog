6102b8fc7b681001d4cb9d60b37d40ecbf38888a
Ted Whang <taeywhang@gmail.com>
2017-07-21 21:50:36 -0700

    V1: Base tracker for dog feeding

---
+++ App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { StyleSheet, Text, View, Switch } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: {
+        sun: { am: false, pm: false },
+        mon: { am: false, pm: false },
+        tue: { am: false, pm: false },
+        wed: { am: false, pm: false },
+        thu: { am: false, pm: false },
+        fri: { am: false, pm: false },
+        sat: { am: false, pm: false }
+      }
+    };
+  }
+
+  updateSwitch(value, dayOfWeek, meridian) {
+    let newState = Object.assign({}, this.state);
+    newState.values[dayOfWeek][meridian] = value;
+    console.log(newState);
+    this.setState(newState);
+  }
+
+  renderDayRow(dayOfWeek) {
+    var meridians = ['am', 'pm'];
+
+    return (
+      <View key={ dayOfWeek } style={ styles.row }>
+        <Text style={ styles.dayOfWeek }>{ dayOfWeek.toUpperCase() }</Text>
+        {meridians.map((meridian) => { return this.renderSwitch(dayOfWeek, meridian) })}
+      </View>
+    );
+  }
+
+  renderSwitch(dayOfWeek, meridian) {
+    return (
+      <View style={styles.switchContainer}>
+        <Switch style={ styles.switch } onValueChange={ (value) => this.updateSwitch(value, dayOfWeek, meridian) } value={ this.state.values[dayOfWeek][meridian] } />
+      </View>
+    )
+  }
+
+  render() {
+    var daysOfWeek = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>DID YOU FEED THE DOG?</Text>
+
+        <View style={styles.rowContainer}>
+          {daysOfWeek.map((day) => { return this.renderDayRow(day) })}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 32,
+    marginBottom: 60
+  },
+  dayOfWeek: {
+    fontSize: 28,
+    flex: 1,
+    textAlign: 'center'
+  },
+  switchContainer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  rowContainer: {
+    width: '50%'
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 18
+  }
+});
